11279229e9b49bd3c2b7e49edfa249c2c9e4ff76
nunesflavio <pnunesflavio@gmail.com>
2026-06-27 14:06:23 -0300

    BD do supabase

--- app.py
+++ app.py
@@ -1,44 +1,68 @@
-#iportando as bibliotecas
-from agno.knowledge.chunking import markdown
+# Importando as bibliotecas
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
 from agno.models.openai import OpenAIChat
 from agno.agent import Agent
 from dotenv import load_dotenv
+from supabase import create_client
+import os
 
-#leitura da chave de api
+# Leitura da chave de API
 load_dotenv()
+# Usando o getenv para pegar o arquivo específico
+supabase_url = os.getenv("SUPABASE_URL")
+# Usando o getenv para pegar o arquivo específico
+supabase_key = os.getenv("SUPABASE_KEY")
+# Criando a conexão com o banco de dados, passando a URL e a KEY.
+supabase = create_client(supabase_url, supabase_key)
 
-#criar o app
+# Criar o nosso app
 app = Flask(__name__)
-
-#habilitar o CORS
+# Habilitar o cors
 CORS(app)
 
-#criar  o agente
+# Criar o agente
 agente = Agent(
     model=OpenAIChat(id="gpt-4o-mini"),
     description="Você é um agente virtual do Hotel Travesseiro Nervoso, slogan: Aqui até a insônia dorme"
-    "Você responde de forma clara e humorada, informações sobre quartos,serviços, reservas e preços"
-    "Quarto Standard ($500), Quarto Deluxe ($700), Quarto Suíte Presidencial ($1000)"
-    "Serviços oferecidos: Academia, Café da Manhã, Lavanderia, Restaurante, Piscina"
-    "Não inclua icones em markdown nas respostas, como: ##, **",
-markdown=True
+                "Você responde de forma clara e humorada, informações sobre quartos,serviços, reservas e preços"
+                "Quarto Standard ($500), Quarto Deluxe ($700), Quarto Suíte Presidencial ($1000)"
+                "Serviços oferecidos: Academia, Café da Manhã, Lavanderia, Restaurante, Piscina"
+                "Não inclua icones em markdown nas respostas, como: ##, **",
+
+    markdown=True
 )
 
-@app.route("/", methods=["GET"])
+
+# Criar a rota VAZIA e o método GET
+@app.route("/", methods=['GET'])
 def testar():
-    return jsonify({"mensage": "API funcionando"})
+    return jsonify({"mensagem": "API funcionando, testes válidos!"})
 
-#criar a rota e o metodos POST
-@app.route("/chat", methods=["POST"])
+
+# Criar a rota e o método POST
+@app.route("/chat", methods=['POST'])
 def pergunta():
-    dados= request.get_json()
-    pergunta = dados["pergunta"]
-
+    dados = request.get_json()
+    pergunta = dados['pergunta']
     resposta = agente.run(pergunta)
-    return jsonify({"resposta":resposta.content})
+    return jsonify({"resposta": resposta.content})
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+
+# Criar a rota para reservas
+@app.route("/reservar", methods=['POST'])
+def reservar():
+    dados = request.get_json()
+    nova_reserva = {
+        "nome": dados["nome"],
+        "email": dados["email"],
+        "check_in": dados["check_in"],
+        "tipo_quarto": dados["tipo_quarto"]
+    }
+    supabase.table("reservas").insert(nova_reserva).execute()
+    return jsonify({"mensagem": "Reserva realizada com sucesso!"})
+
+
+# Rodar o nosso app
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000)
